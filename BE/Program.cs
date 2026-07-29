92c2c8d953fcd724a15d146b309d1df55b16c0c0
Duy Vu 2kxx <buitranduyvu2002@gmail.com>
2026-07-29 18:26:59 +0700

    chore: remove startup SQL migration block now that schema changes are permanently applied

--- BE/Program.cs
+++ BE/Program.cs
@@ -52,57 +52,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Auto-ensure IsQrUnlocked column exists in Bookings table
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    try
-    {
-        context.Database.ExecuteSqlRaw(@"
-            IF NOT EXISTS (
-                SELECT * FROM sys.columns 
-                WHERE object_id = OBJECT_ID(N'[Bookings]') 
-                AND name = 'IsQrUnlocked'
-            )
-            BEGIN
-                ALTER TABLE [Bookings] ADD [IsQrUnlocked] BIT NOT NULL DEFAULT 0;
-            END
-
-            IF NOT EXISTS (
-                SELECT * FROM sys.columns 
-                WHERE object_id = OBJECT_ID(N'[Bookings]') 
-                AND name = 'TotalPrice'
-            )
-            BEGIN
-                ALTER TABLE [Bookings] ADD [TotalPrice] DECIMAL(18,2) NOT NULL DEFAULT 0;
-            END
-
-            IF NOT EXISTS (
-                SELECT * FROM sys.columns 
-                WHERE object_id = OBJECT_ID(N'[Tents]') 
-                AND name = 'IsQrUnlocked'
-            )
-            BEGIN
-                ALTER TABLE [Tents] ADD [IsQrUnlocked] BIT NOT NULL DEFAULT 0;
-            END
-
-            IF EXISTS (
-                SELECT * FROM sys.columns 
-                WHERE object_id = OBJECT_ID(N'[Orders]') 
-                AND name = 'BookingId'
-                AND is_nullable = 0
-            )
-            BEGIN
-                ALTER TABLE [Orders] ALTER COLUMN [BookingId] INT NULL;
-            END
-        ");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Error auto-adding IsQrUnlocked column: " + ex.Message);
-    }
-}
-
 // Map SignalR Hubs
 app.MapHub<OrderHub>("/orderHub");
 
